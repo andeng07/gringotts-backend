c7946f38b3359ff5b20dc1f480d899c7377843ec
andeng07 <sumilangandreijohn@gmail.com>
2024-11-25 21:49:13 +0800

    feat: added a UserService function to create a new user

--- Gringotts.Api/Shared/Services/UserService.cs
+++ Gringotts.Api/Shared/Services/UserService.cs
@@ -1,12 +1,33 @@
 using Gringotts.Api.Shared.Database;
 using Gringotts.Api.Shared.Database.Models.Users;
+using Gringotts.Api.Shared.Results;
 using Microsoft.EntityFrameworkCore;
 
 namespace Gringotts.Api.Shared.Services;
 
 public class UserService(AppDbContext dbContext)
 {
-    
+    // TODO: finalize the data being stored
+    public async Task<TypedResult<Guid>> CreateUser(string cardId, string schoolId, string firstName, string lastName, string? middleName = null)
+    {
+        var userId = Guid.NewGuid();
+
+        var newUser = new User
+        {
+            Id = userId,
+            CardId = cardId,
+            SchoolId = schoolId,
+            FirstName = firstName,
+            LastName = lastName,
+            MiddleName = middleName
+        };
+        
+        dbContext.Users.Add(newUser);
+
+        await dbContext.SaveChangesAsync();
+
+        return userId;
+    }
+        
     public async Task<bool> IsUserRegistered(Guid id) => await dbContext.Users.AnyAsync(x => x.Id == id);
-    
 }
