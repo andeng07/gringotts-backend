17357b67c322f83d9b6517207620f47b262db96e
andeng07 <sumilangandreijohn@gmail.com>
2024-11-25 21:47:26 +0800

    refactor: renamed package from *.*.*.UserSecrets.* to *.*.*.UserAuthentication.*

---
+++ Gringotts.Api/Features/UserAuthentication/Endpoints/RegisterUserEndpoint.cs
@@ -0,0 +1,72 @@
+using Gringotts.Api.Features.UserAuthentication.Services;
+using Gringotts.Api.Shared.Endpoints;
+using Gringotts.Api.Shared.Results;
+using Gringotts.Api.Shared.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gringotts.Api.Features.UserAuthentication.Endpoints;
+
+public class RegisterUserEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGroup("/user-auth").MapPost("/register", HandleAsync)
+            .AddEndpointFilter<ValidationEndpointFilter<RegisterUserRequest>>();
+    }
+
+    private record RegisterUserRequest(
+        string Email,
+        string CardId,
+        string SchoolId,
+        string FirstName,
+        string LastName,
+        string Password,
+        string? MiddleName = null
+    );
+
+    private record RegisterUserResponse(Guid UserId, string Email);
+
+    private static async Task<TypedResult<RegisterUserResponse>> HandleAsync(
+        [FromBody] RegisterUserRequest request,
+        UserSecretService userSecretService,
+        UserService userService,
+        CancellationToken cancellationToken
+    )
+    {
+        if (await userSecretService.HasSecretAsync(request.Email))
+        {
+            return TypedResult<RegisterUserResponse>.Failure(new Error(
+                "Authentication.Register.EmailAlreadyRegistered",
+                "An account with this email address already exists.",
+                Error.ErrorType.Conflict
+            ));
+        }
+
+        var createSecretResult = await userSecretService.CreateSecretAsync(
+            request.Email,
+            request.Password
+        );
+
+        if (!createSecretResult.IsSuccess)
+        {
+            return TypedResult<RegisterUserResponse>.Failure(createSecretResult.Errors.ToArray());
+        }
+        
+        var registeredUserResult = await userService.CreateUser(
+            request.CardId,
+            request.SchoolId,
+            request.FirstName,
+            request.LastName,
+            request.MiddleName
+        );
+
+        if (!registeredUserResult.IsSuccess)
+        {
+            return TypedResult<RegisterUserResponse>.Failure(registeredUserResult.Errors.ToArray());
+        }
+        
+        var response = new RegisterUserResponse(registeredUserResult.Value, request.Email);
+        
+        return TypedResult<RegisterUserResponse>.Success(response);
+    }
+}
