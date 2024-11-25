9881ec9109b6b2901f6e30fec79656e743074fc8
andeng07 <sumilangandreijohn@gmail.com>
2024-11-25 21:48:46 +0800

    refactor: added CardId property to User data model

--- Gringotts.Api/Shared/Database/Models/Users/User.cs
+++ Gringotts.Api/Shared/Database/Models/Users/User.cs
@@ -3,6 +3,7 @@ namespace Gringotts.Api.Shared.Database.Models.Users
     public record User
     {
         public required Guid Id { get; init; }
+        public required string CardId { get; set; }
         public required string SchoolId { get; set; }
         public required string FirstName { get; set; }
         public required string? MiddleName { get; set; }
