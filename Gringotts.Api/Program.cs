1d5c9851f6e1673c190de9586b2116f0c2ec76d8
andeng07 <sumilangandreijohn@gmail.com>
2024-12-15 22:51:14 +0800

    chore: register UserJwtService, UserSecretService, and UserService as singletons

--- Gringotts.Api/Program.cs
+++ Gringotts.Api/Program.cs
@@ -1,10 +1,17 @@
 using FluentValidation;
+using Gringotts.Api.Features.UserAuthentication.Services;
 using Gringotts.Api.Shared.Endpoints;
+using Gringotts.Api.Shared.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddEndpoints();
+
+
+builder.Services.AddSingleton<UserJwtService>();
+builder.Services.AddSingleton<UserSecretService>();
+builder.Services.AddSingleton<UserService>();
 
 if (builder.Environment.IsDevelopment()) {
     builder.Services.AddEndpointsApiExplorer();
