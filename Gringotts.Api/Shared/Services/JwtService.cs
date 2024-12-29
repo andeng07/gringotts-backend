47ab0863af451b1aa5fadec8369ec65b9d9437ab
andeng07 <sumilangandreijohn@gmail.com>
2024-12-30 00:21:53 +0800

    docs: add code documentation

--- Gringotts.Api/Shared/Services/JwtService.cs
+++ Gringotts.Api/Shared/Services/JwtService.cs
@@ -7,12 +7,23 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace Gringotts.Api.Shared.Services;
 
+/// <summary>
+/// An abstract class providing functionality for generating and validating JSON Web Tokens (JWT).
+/// It handles JWT creation with claims and secret keys as well as validating the token's authenticity and expiration.
+/// </summary>
+/// <param name="configuration">The configuration containing JWT settings such as secret, issuer, and audience.</param>
 public abstract class JwtService(IConfiguration configuration)
 {
     private readonly string _secret = configuration["Jwt:Secret"] ?? throw new ConfigurationMissingFieldException("Jwt:Secret");
     private readonly string _issuer = configuration["Jwt:Issuer"] ?? throw new ConfigurationMissingFieldException("Jwt:Issuer");
     private readonly string _audience = configuration["Jwt:Audience"] ?? throw new ConfigurationMissingFieldException("Jwt:Audience");
     
+    /// <summary>
+    /// Generates a JWT token based on the specified claims and optional expiration time.
+    /// </summary>
+    /// <param name="claims">The claims to be included in the JWT token.</param>
+    /// <param name="expires">Optional expiration date for the token. If not provided, the token will not expire.</param>
+    /// <returns>A string representing the generated JWT token.</returns>
     protected string GenerateToken(IEnumerable<Claim> claims, DateTime? expires = null)
     {
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret));
@@ -28,6 +39,12 @@ public abstract class JwtService(IConfiguration configuration)
         return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
     }
 
+    /// <summary>
+    /// Validates the provided JWT token and returns the claims principal if valid.
+    /// </summary>
+    /// <param name="token">The JWT token to be validated.</param>
+    /// <param name="validateLifetime">A flag indicating whether to validate the token's expiration time.</param>
+    /// <returns>A <see cref="TypedResult{ClaimsPrincipal}"/> containing the validated claims principal or an error result if validation fails.</returns>
     protected TypedResult<ClaimsPrincipal> ValidateToken(string token, bool validateLifetime)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
