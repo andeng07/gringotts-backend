3dffc75e3a1bbd50adf7df0d9d4b51da988ea183
andeng07 <sumilangandreijohn@gmail.com>
2024-12-30 21:54:24 +0800

    feat: add error codes classes for Authentication and Validation error codes

---
+++ Gringotts.Api/Shared/Errors/ErrorCodes.cs
@@ -0,0 +1,14 @@
+﻿namespace Gringotts.Api.Shared.Errors;
+
+public static class AuthErrorCodes
+{
+    public const string CredentialsInvalid = "Auth.Credentials.Invalid";  
+}
+
+public static class ValidationErrorCodes
+{
+    public const string EmailRequired = "Validation.Email.Required";
+    public const string InvalidEmailFormat = "Validation.Email.InvalidFormat"; 
+    public const string PasswordTooShort = "Validation.Password.TooShort";
+    public const string PasswordRequired = "Validation.Password.Required";
+}
