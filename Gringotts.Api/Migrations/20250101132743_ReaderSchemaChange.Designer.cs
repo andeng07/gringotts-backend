b71507f17bb93cc07999e9d0b9d62d92556db14f
andeng07 <sumilangandreijohn@gmail.com>
2025-01-01 22:04:43 +0800

    chore: database migration

--- Gringotts.Api/Migrations/20250101132743_ReaderSchemaChange.Designer.cs
+++ Gringotts.Api/Migrations/20250101132743_ReaderSchemaChange.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Gringotts.Api.Shared.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Gringotts.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250101132743_ReaderSchemaChange")]
+    partial class ReaderSchemaChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Gringotts.Api/Migrations/20250101132743_ReaderSchemaChange.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gringotts.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReaderSchemaChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReaderSecrets");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccessToken",
+                table: "Readers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoomName",
+                table: "Locations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessToken",
+                table: "Readers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoomName",
+                table: "Locations",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ReaderSecrets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccessToken = table.Column<string>(type: "text", nullable: false),
+                    ReaderId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReaderSecrets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReaderSecrets_Readers_ReaderId",
+                        column: x => x.ReaderId,
+                        principalTable: "Readers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReaderSecrets_ReaderId",
+                table: "ReaderSecrets",
+                column: "ReaderId",
+                unique: true);
+        }
+    }
+}
