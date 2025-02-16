fb2b1b9bdb92d91a9901b160b1898bb0258ee787
andeng07 <sumilangandreijohn@gmail.com>
2025-02-16 12:06:59 +0800

    feat: add endpoints for retrieving logs, sessions, and active sessions

---
+++ Gringotts.Api/Features/Interactions/Endpoints/GetInteractionLogsEndpoint.cs
@@ -0,0 +1,62 @@
+﻿using Gringotts.Api.Features.Interactions.DataFilters;
+using Gringotts.Api.Features.Interactions.Models;
+using Gringotts.Api.Shared.Core;
+using Gringotts.Api.Shared.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gringotts.Api.Features.Interactions.Endpoints;
+
+public class GetInteractionLogsEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/logs/filter", async (
+            [FromBody] GetInteractionLogsRequest request,
+            AppDbContext dbContext
+        ) =>
+        {
+            var filter = new InteractionLogsFilter(
+                request.UserIds,
+                request.ReaderIds,
+                request.From,
+                request.To,
+                request.Type
+            ).ApplyFilters();
+
+            return await EndpointHelpers.GetEntities<InteractionLog, GetInteractionLogResponse>(
+                dbContext,
+                request.Page,
+                request.PageSize,
+                filter,
+                entity =>
+                    new GetInteractionLogResponse(
+                        entity.Id,
+                        entity.LogReaderId,
+                        entity.LogUserId,
+                        entity.CardId,
+                        entity.DateTime,
+                        entity.InteractionType
+                    )
+            );
+        });
+    }
+
+    public record GetInteractionLogsRequest(
+        int Page,
+        int PageSize,
+        IEnumerable<Guid>? UserIds,
+        IEnumerable<Guid>? ReaderIds,
+        DateTime? From,
+        DateTime? To,
+        InteractionType? Type
+    );
+
+    public record GetInteractionLogResponse(
+        Guid Id,
+        Guid LogReaderId,
+        Guid? LogUserId,
+        string CardId,
+        DateTime DateTime,
+        InteractionType Type
+    );
+}
---
+++ Gringotts.Api/Features/Interactions/Endpoints/GetSessionsEndpoint.cs
@@ -0,0 +1,58 @@
+﻿using Gringotts.Api.Features.Interactions.DataFilters;
+using Gringotts.Api.Features.Interactions.Models;
+using Gringotts.Api.Shared.Core;
+using Gringotts.Api.Shared.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gringotts.Api.Features.Interactions.Endpoints;
+
+public class GetSessionsEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+{
+        app.MapPost("/sessions/filter", async (
+            [FromBody] GetSessionsRequest request,
+            AppDbContext dbContext
+        ) =>
+        {
+            var filter = new SessionsFilter(
+                request.UserIds,
+                request.ReaderIds,
+                request.From,
+                request.To
+            ).ApplyFilters();
+            
+            return await EndpointHelpers.GetEntities<Session, GetSessionResponse>(
+                dbContext,
+                request.Page,
+                request.PageSize,
+                filter,
+                entity =>
+                    new GetSessionResponse(
+                        entity.Id,
+                        entity.LogReaderId,
+                        entity.LogUserId,
+                        entity.StartDate,
+                        entity.EndDate
+                    )
+            );
+        });
+    }
+
+    public record GetSessionsRequest(
+        int Page,
+        int PageSize,
+        IEnumerable<Guid>? UserIds,
+        IEnumerable<Guid>? ReaderIds,
+        DateTime? From,
+        DateTime? To
+    );
+
+    public record GetSessionResponse(
+        Guid Id,
+        Guid LogReaderId,
+        Guid LogUserId,
+        DateTime StartDate,
+        DateTime EndDate
+    );
+}
