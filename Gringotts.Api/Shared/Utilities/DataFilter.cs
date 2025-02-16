26973e1933fb23e09ef66090d9f3b4234e56f610
andeng07 <sumilangandreijohn@gmail.com>
2025-02-16 12:05:58 +0800

    feat: add helper class to streamline making data queries

---
+++ Gringotts.Api/Shared/Utilities/DataFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+using Gringotts.Api.Shared.Core;
+using LinqKit;
+
+namespace Gringotts.Api.Shared.Utilities;
+
+public class DataFilter<T>
+{
+    private ExpressionStarter<T> _predicate = PredicateBuilder.New<T>(true); // Default to 'true' (no filtering)
+
+    public DataFilter<T> Where(Expression<Func<T, bool>> predicate)
+    {
+        _predicate = _predicate.And(predicate);
+        return this;
+    }
+
+    public Expression<Func<T, bool>> Build() => _predicate;
+}
+
+public interface IDataFilter<T> where T : class, IEntity
+{
+    Expression<Func<T, bool>> ApplyFilters();
+}
+
+public static class FilteredQueryExtensions
+{
+    public static IQueryable<T> ApplyDataFilters<T>(this IQueryable<T> query, Expression<Func<T, bool>> filterExpression)
+    {
+        return query.AsExpandable().Where(filterExpression);
+    }
+}
